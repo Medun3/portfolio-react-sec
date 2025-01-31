1dbc530e283a9b08e907ab46833ddabc46a12941
Medun3 <medunraj3@gmail.com>
2025-01-31 18:43:53 +0530

    Updato

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import App from './App.jsx'
-import Intro from "./intro.jsx"
-import Credentials from './Credentials.jsx'
-import Project from './project.jsx'
-import Contact from './contact.jsx'
+import Intro from "./component/intro.jsx"
+import Credentials from './component/Credentials.jsx'
+import Project from './component/project.jsx'
+import Contact from './component/contact.jsx'
 
-import Html from './credentailsFile/html.jsx'
-import Other from './credentailsFile/other.jsx'
-import Javascript from './credentailsFile/javasrcipt.jsx'
-import ReactJS from './credentailsFile/react.jsx'
-import Node from './credentailsFile/node.jsx'
-import Express from './credentailsFile/express.jsx'
-import MongoDB from './credentailsFile/mongodb.jsx'
-import Git from './credentailsFile/git.jsx'
-import C from './credentailsFile/c.jsx'
+import Html from './component/credentailsFile/html.jsx'
+import Other from './component/credentailsFile/other.jsx'
+import Javascript from './component/credentailsFile/javasrcipt.jsx'
+import ReactJS from './component/credentailsFile/react.jsx'
+import Node from './component/credentailsFile/node.jsx'
+import Express from './component/credentailsFile/express.jsx'
+import MongoDB from './component/credentailsFile/mongodb.jsx'
+import Git from './component/credentailsFile/git.jsx'
+import C from './component/credentailsFile/c.jsx'
 
 
 createRoot(document.getElementById('root')).render(
